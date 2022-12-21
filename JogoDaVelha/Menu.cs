bd049f601617cac0441da0ff8e3908181e1e75cb
Luis Guilherme Vieira Neves <luisguilhermevnevesnogueira@gmail.com>
2022-12-21 06:32:27 -0300

    Fix migrando o menu que estava no método main para a classe Menu

--- JogoDaVelha/Menu.cs
+++ JogoDaVelha/Menu.cs
@@ -8,6 +8,9 @@ namespace JogoDaVelha
 {
     internal class Menu
     {
+        public static string escolhaUsuario;
+
+
 
         // # Método mostrar Menu
         public static void MostrarMenu()
@@ -16,6 +19,28 @@ namespace JogoDaVelha
             Console.BackgroundColor= ConsoleColor.Red;
             Console.WriteLine("-----------------  BEM-VINDO AO JOGO DA VELHA  -----------------\n");
             Console.ResetColor();
+
+            // # True até ser usuário não querer jogar 
+            while (true)
+            {
+                Console.Write("Deseja jogar o jogo da velha? SIM ou NÃO: ");
+                escolhaUsuario = Console.ReadLine().ToUpper();
+
+                if (escolhaUsuario == "SIM")
+                {
+                    Console.WriteLine("Jogando");
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n------ Encerrando aplicação ------");
+                    Console.ResetColor();
+                    break;
+                }
+
+            }
+
         }
     }
 }
--- JogoDaVelha/Program.cs
+++ JogoDaVelha/Program.cs
@@ -4,29 +4,11 @@
     {
         public static void Main(string[] args) 
         {
+            
+            // # Mostra Interface ao usuário
+            Menu.MostrarMenu();
 
-            string escolhaUsuario;
 
-            while (true)
-            {
-                Menu.MostrarMenu();
-
-                Console.Write("Deseja jogar o jogo da velha? SIM ou NÃO: ");
-                escolhaUsuario = Console.ReadLine().ToUpper();
-
-                if(escolhaUsuario == "SIM")
-                {
-                    Console.WriteLine("Jogando");
-                    break;
-                }
-                else
-                {
-                    Console.ForegroundColor= ConsoleColor.Red;
-                    Console.WriteLine("\n------ Encerrando aplicação ------");
-                    Console.ResetColor();
-                    break;
-                }
-            }
 
             
         }

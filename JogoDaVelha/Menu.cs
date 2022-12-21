8b1844e04be09b95f36145272c2c8e037f87e999
Luis Guilherme Vieira Neves <luisguilhermevnevesnogueira@gmail.com>
2022-12-21 06:25:15 -0300

    Add Criação da classe Menu e método mostrar Menu, criação do começo do jogo com input do usuário

---
+++ JogoDaVelha/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaVelha
+{
+    internal class Menu
+    {
+
+        // # Método mostrar Menu
+        public static void MostrarMenu()
+        {
+            Console.ForegroundColor= ConsoleColor.Black;
+            Console.BackgroundColor= ConsoleColor.Red;
+            Console.WriteLine("-----------------  BEM-VINDO AO JOGO DA VELHA  -----------------\n");
+            Console.ResetColor();
+        }
+    }
+}
--- JogoDaVelha/Program.cs
+++ JogoDaVelha/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace JogoDaVelha
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+
+            string escolhaUsuario;
+
+            while (true)
+            {
+                Menu.MostrarMenu();
+
+                Console.Write("Deseja jogar o jogo da velha? SIM ou NÃO: ");
+                escolhaUsuario = Console.ReadLine().ToUpper();
+
+                if(escolhaUsuario == "SIM")
+                {
+                    Console.WriteLine("Jogando");
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("\n------ Encerrando aplicação ------");
+                    Console.ResetColor();
+                    break;
+                }
+            }
+
+            
+        }
+    }
+}

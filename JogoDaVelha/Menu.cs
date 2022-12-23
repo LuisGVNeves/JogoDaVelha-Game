8be66aeec312ee65c5d16bd16f5e801896bb6de1
Luis Guilherme Vieira Neves <luisguilhermevnevesnogueira@gmail.com>
2022-12-23 07:08:49 -0300

    Fix Loop infinito após acabar a partida no método iniciarJogo

--- JogoDaVelha/Menu.cs
+++ JogoDaVelha/Menu.cs
@@ -8,6 +8,38 @@ namespace JogoDaVelha
 {
     internal class Menu
     {
+        // # Instanciação dos jogadores
+        public static Jogador jogador1 = new Jogador("", "", 0);
+        public static Jogador jogador2 = new Jogador("", "", 0);
+
+
+        // # Iniciar entrada do Game
+        public static void IniciarEntradaDoJogo()
+        {
+            Console.Write("\nDigite o nome do 1 jogador: ");
+            jogador1.nome = Console.ReadLine();
+
+            Console.Write("Digite o nome do 2 jogador: ");
+            jogador2.nome = Console.ReadLine();
+
+            Console.Write("\nJogador 1 qual você quer ser? letra X ou O: ");
+            jogador1.letraJogo = Console.ReadLine().ToUpper();
+
+            if (jogador1.letraJogo == "X")
+            {
+                jogador2.letraJogo = "O";
+            }
+            else
+            {
+                jogador1.letraJogo = "O";
+                jogador2.letraJogo = "X";
+            }
+
+            Console.WriteLine($"\nJogador {jogador1.nome} começa com: {jogador1.letraJogo}");
+            Console.WriteLine($"\nJogador {jogador2.nome} começa com: {jogador2.letraJogo}\n\n");
+        }
+
+
         // # Variável escolha do usuário
         public static string escolhaUsuario;
 
@@ -23,7 +55,7 @@ namespace JogoDaVelha
                 if (escolhaUsuario == "SIM")
                 {
                     // # Iniciar a entrada inicial do Game
-                    Tabuleiro.IniciarEntradaDoJogo();
+                    IniciarEntradaDoJogo();
 
                     // # Preencher o tabuleiro com os números
                     Tabuleiro.PreencherTabuleiro();
--- JogoDaVelha/Tabuleiro.cs
+++ JogoDaVelha/Tabuleiro.cs
@@ -9,41 +9,11 @@ namespace JogoDaVelha
 {
     internal class Tabuleiro
     {
-        // # Instanciação dos jogadores
-        public static Jogador jogador1 = new Jogador("","", 0);
-        public static Jogador jogador2 = new Jogador("","", 0);
-
-
-        // # Iniciar entrada do Game
-        public static void IniciarEntradaDoJogo()
-        {
-            Console.Write("\nDigite o nome do 1 jogador: ");
-            jogador1.nome = Console.ReadLine();
-
-            Console.Write("Digite o nome do 2 jogador: ");
-            jogador2.nome = Console.ReadLine();
-            
-            Console.Write("\nJogador 1 qual você quer ser? letra X ou O: ");
-            jogador1.letraJogo = Console.ReadLine().ToUpper();
-
-            if(jogador1.letraJogo == "X")
-            {
-                jogador2.letraJogo = "O";
-            }
-            else
-            {
-                jogador1.letraJogo = "O";
-                jogador2.letraJogo = "X";
-            }
-
-            Console.WriteLine($"\nJogador {jogador1.nome} começa com: {jogador1.letraJogo}");
-            Console.WriteLine($"\nJogador {jogador2.nome} começa com: {jogador2.letraJogo}\n\n");
-        }
-
         // # Criar a Matriz - 3 linhas e 3 colunas
         public static string[,] tabuleiro = new string[3, 3];
 
-        // # Preencher Tabuleiro com os números
+
+        // # Método que preenche tabuleiro com números, para o usuário preencher com X ou O
         public static void PreencherTabuleiro()
         {
             for (int linhas = 0; linhas<3; linhas++)
@@ -69,24 +39,27 @@ namespace JogoDaVelha
 		    }
         }
 
-        // # Mostrar Tabuleiro
+
+        // # Método Mostrar Tabuleiro
         public static void MostrarTabuleiro()
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($" {tabuleiro[0,0]}  | {tabuleiro[0,1]} |  {tabuleiro[0,2]}");
             Console.WriteLine($" {tabuleiro[1,0]}  | {tabuleiro[1,1]} |  {tabuleiro[1,2]}");
-            Console.WriteLine($" {tabuleiro[2,0]}  | {tabuleiro[2,1]} |  {tabuleiro[2,2]}");
+            Console.WriteLine($" {tabuleiro[2,0]}  | {tabuleiro[2,1]} |  {tabuleiro[2,2]}\n");
             Console.ResetColor();
-
-
         }
 
-        // # Variável publica para verificar a matriz
+
+        // # Variável publica para verificar a matriz na horizontal
         public static bool linhaHorizontalX;
-        
+        public static bool linhaHorizontalO;
+
+
         // # Método que verifica se a vitória foi horizontal
         public static void VerificaVitoriaHorizontal()
         {
+            // Percorro a matriz e verifico se as linhas estão X X X  ou  O O O, e vai retornar um booleano true ou false
             for (int linhas = 0; linhas < 3; linhas++)
             {
                 for (int colunas = 0; colunas < 3; colunas++)
@@ -114,34 +87,49 @@ namespace JogoDaVelha
                         tabuleiro[2, 2] == "X"
                      );
 
+                    /*
+                      # Verifica se a primeira, segunda, terceira linha da matriz
+                      teve as linhas prenchidas na ordem orizontal O O O
+                     */
+                    linhaHorizontalO =
+                    (
+                        tabuleiro[0, 0] == "O" &&
+                        tabuleiro[0, 1] == "O" &&
+                        tabuleiro[0, 2] == "O"
+                    )
+                    ||
+                    (
+                        tabuleiro[1, 0] == "O" &&
+                        tabuleiro[1, 1] == "O" &&
+                        tabuleiro[1, 2] == "O"
+                     )
+                     ||
+                     (
+                        tabuleiro[2, 0] == "O" &&
+                        tabuleiro[2, 1] == "O" &&
+                        tabuleiro[2, 2] == "O"
+                     );
+
                 }
-            }
-            if (jogador1.letraJogo == "X" && linhaHorizontalX)
-            {
-                Console.WriteLine($"\nJogador {jogador1.nome} Venceu a partida :D");
-            }
-            if(jogador2.letraJogo == "X" && linhaHorizontalX)
-            {
-                Console.WriteLine($"\nJogador {jogador2.nome} Venceu a partida :D");
             }
         }
 
-
+        // # Método que inicia jogo
         public static void IniciarJogo()
         {
-            bool verificador = true;
-            while (verificador)
+            while (true)
             {
                 // # Dar opção pro usuário qual valor da matriz ele quer prencher :
-                Console.Write($"\nJogador 1 {jogador1.nome} - Escolha o valor do tabuleiro que você quer preencher {jogador1.letraJogo}: ");
+
+                Console.Write($"\nJogador 1 {Menu.jogador1.nome} - Escolha o valor do tabuleiro que você quer preencher {Menu.jogador1.letraJogo}: ");
                 string escolhaJogador1 = Console.ReadLine();
 
-                Console.Write($"\nJogador 2 {jogador2.nome} - Escolha o valor do tabuleiro que você quer preencher com {jogador2.letraJogo}: ");
+                Console.Write($"\nJogador 2 {Menu.jogador2.nome} - Escolha o valor do tabuleiro que você quer preencher com {Menu.jogador2.letraJogo}: ");
                 string escolhaJogador2 = Console.ReadLine();
 
                 /*
                     - Aqui eu vou percorrer a matriz, e comparar e ver se é igual a escolha do jogador
-                    se for, eu vou substituir o número pela letra e mostrar o tabuleiro de novo
+                    se for, eu vou substituir o número pela letra
                 */
                 for (int linhas = 0; linhas < 3; linhas++)
                 {
@@ -149,22 +137,45 @@ namespace JogoDaVelha
                     {
                         if (tabuleiro[linhas,colunas] == escolhaJogador1)
                         {
-                            tabuleiro[linhas, colunas] = jogador1.letraJogo;
+                            tabuleiro[linhas, colunas] = Menu.jogador1.letraJogo;
                         }
                         if (tabuleiro[linhas, colunas] == escolhaJogador2)
                         {
-                            tabuleiro[linhas, colunas] = jogador2.letraJogo;
+                            tabuleiro[linhas, colunas] = Menu.jogador2.letraJogo;
                         }
                     }
                 }
 
-                // # Quando matriz for trocada de número por X ou O, a função mostra o tabuleiro para mostrar como está o jogo
+
+                // # Quando matriz for trocada de número por X ou O, invoco a função mostra o tabuleiro para mostrar como está o jogo atual
                 MostrarTabuleiro();
 
-                // # Métodos que verificam os preenchimentos e vitória, se tudo tiver bem, break loop infinito
-                VerificaVitoriaHorizontal();
-                
 
+                // # Método que verifica se a vitória é horizontal
+                VerificaVitoriaHorizontal();
+
+
+                // # Mostra mensagem caso a vitória tenha sido horizontal tanto de X como de O
+                if (Menu.jogador1.letraJogo == "X" && linhaHorizontalX)
+                {
+                    Console.WriteLine($"\nJogador {Menu.jogador1.nome} Venceu a partida :D");
+                    break;
+                }
+                if (Menu.jogador1.letraJogo == "O" && linhaHorizontalO)
+                {
+                    Console.WriteLine($"\nJogador {Menu.jogador1.nome} Venceu a partida :D");
+                    break;
+                }
+                if (Menu.jogador2.letraJogo == "X" && linhaHorizontalX)
+                {
+                    Console.WriteLine($"\nJogador {Menu.jogador2.nome} Venceu a partida :D");
+                    break;
+                }
+                if (Menu.jogador2.letraJogo == "O" && linhaHorizontalO)
+                {
+                    Console.WriteLine($"\nJogador {Menu.jogador2.nome} Venceu a partida :D");
+                    break;
+                }
             }
 
 

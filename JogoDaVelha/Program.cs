79b881421b6d792fbafb22bb7db896992cd2cc55
Luis Guilherme Vieira Neves <luisguilhermevnevesnogueira@gmail.com>
2022-12-31 03:39:30 -0300

    Fix Retirei o método de mostrarInterface que estava sendo desnecessário no método principal, e joguei o código direto no main do código principal

--- JogoDaVelha/Program.cs
+++ JogoDaVelha/Program.cs
@@ -9,9 +9,40 @@ namespace JogoDaVelha
         public static Jogador jogador1 = new Jogador("", "", 0);
         public static Jogador jogador2 = new Jogador("", "", 0);
 
+        // # Variável para parar o looping infinito no método iniciarJogo e começar uma nova partida
+        public static string jogarDeNovo;
 
-        // # Método Mostrar a interface inicial do jogo
-        public static void MostrarInterfaceJogo()
+        // # Método que inicia o jogo
+        public static void IniciarJogo()
+        {
+            // Preenche a matriz com números [0-9]
+            PreencherTabuleiro();
+
+            do
+            {
+                // # Quando matriz for trocada de número [0-9] por X ou O, invoco a função mostra o tabuleiro para mostrar como está o jogo atual
+                MostrarTabuleiro();
+
+                // Método para perguntar ao usuário se ele deseja X ou O para preencher na matriz e trocar os números da matriz pela escolha do jogar X ou O
+                EscolhaJogador(jogarDeNovo);
+
+                // # Método que verifica se deu velha
+                VerificaEmpate(jogarDeNovo);
+
+                // # Método que verifica se a vitória é diagonal e pergunta se deseja jogar de novo
+                VerificaVitoriaDiagonal(jogarDeNovo);
+                
+                // # Método que verifica se a vitória é horizontal e pergunta se deseja jogar de novo
+                VerificaVitoriaHorizontal(jogarDeNovo);
+
+                // # Método que verifica se a vitória é vertical e pergunta se deseja jogar de novo
+                VerificaVitoriaVertical(jogarDeNovo);
+            } while (jogarDeNovo != "NAO" && jogarDeNovo != "NÃO");
+
+        }
+
+
+        public static void Main(string[] args)
         {
             // # Menu inicial
             EstilizarMenu("BEM-VINDO AO JOGO DA VELHA", ConsoleColor.Red);
@@ -62,47 +93,5 @@ namespace JogoDaVelha
             while (escolhaUsuario != "NAO" && escolhaUsuario != "NÃO");
 
         }
-
-        // # Variável para parar o looping infinito no método iniciarJogo e começar uma nova partida
-        public static string jogarDeNovo;
-
-
-        // # Método que inicia o jogo
-        public static void IniciarJogo()
-        {
-            // Preenche a matriz com números [0-9]
-            PreencherTabuleiro();
-
-            do
-            {
-                // # Quando matriz for trocada de número [0-9] por X ou O, invoco a função mostra o tabuleiro para mostrar como está o jogo atual
-                MostrarTabuleiro();
-
-                // Método para perguntar ao usuário se ele deseja X ou O para preencher na matriz e trocar os números da matriz pela escolha do jogar X ou O
-                EscolhaJogador(jogarDeNovo);
-
-                // # Método que verifica se deu velha
-                VerificaEmpate(jogarDeNovo);
-
-                // # Método que verifica se a vitória é diagonal e pergunta se deseja jogar de novo
-                VerificaVitoriaDiagonal(jogarDeNovo);
-                
-                // # Método que verifica se a vitória é horizontal e pergunta se deseja jogar de novo
-                VerificaVitoriaHorizontal(jogarDeNovo);
-
-                // # Método que verifica se a vitória é vertical e pergunta se deseja jogar de novo
-                VerificaVitoriaVertical(jogarDeNovo);
-            } while (jogarDeNovo != "NAO" && jogarDeNovo != "NÃO");
-
-        }
-
-
-        public static void Main(string[] args)
-        {
-            MostrarInterfaceJogo();
-        }
-
     }
-
-
 }
